5870515e7d643e57523a2d9fa30b53e8e4c6d62d
ASPP Student <student@localhost>
2021-08-24 12:39:28 +0200

    implemented question b: iterate_f+test

--- logistic.py
+++ logistic.py
@@ -1,3 +1,12 @@
 def logistic_map(r,x):
 
     return r*x*(1-x)
+
+def iterate_f(it, r, x):
+
+    out = [logistic_map(r,x)]
+
+    for i in range(it-1):
+        out.append(logistic_map(r,out[-1]))
+
+    return out
--- test_logistic.py
+++ test_logistic.py
@@ -1,4 +1,4 @@
-from logistic import logistic_map
+from logistic import logistic_map, iterate_f
 import numpy as np
 import pytest
 
@@ -7,3 +7,10 @@ def test_logistic_map(r, x, expected):
     x = logistic_map(r, x)
 
     assert np.isclose(x, expected)
+
+@pytest.mark.parametrize("it, r, x, expected", [[1, 2.2, 0.1, [0.198]], [4, 3.4,0.2, [0.544, 0.843418, 0.449019, 0.841163]], [2, 1.7,0.75, [0.31875,0.369152]]])
+def test_iterate_f(it, r, x, expected):
+
+    lst = iterate_f(it, r, x)
+
+    assert np.allclose(lst, expected)
